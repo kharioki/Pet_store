0c872f9572432c56c707c0dadfb4096c7728f777
kharioki <antolisah@gmail.com>
2020-06-12 02:37:00 +0300

    preconfiguration

---
+++ lib/configuration.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+Color primaryGreen = Color(0xFF416D6D);
+
+List<BoxShadow> shadowList = [
+  BoxShadow(color: Colors.grey[300], blurRadius: 30, offset: Offset(0, 10)),
+];
+
+List<Map> categories = [
+  {'name': 'Cats', 'iconPath': 'assets/images/cat.png'},
+  {'name': 'Dogs', 'iconPath': 'assets/images/dog.png'},
+  {'name': 'Bunnies', 'iconPath': 'assets/images/rabbit.png'},
+  {'name': 'Parrots', 'iconPath': 'assets/images/parrot.png'},
+  {'name': 'Horses', 'iconPath': 'assets/images/horse.png'},
+];
